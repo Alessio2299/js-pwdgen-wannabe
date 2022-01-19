59fbcc8915732b914cbb4255027409253b43ba81
Alessio Napoli <alessionapoli99@icloud.com>
2022-01-19 14:20:20 +0100

    Fix

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let name = prompt("Qual'è il tuo nome?");
 
 let surname = prompt("Qual'è il tuo cognome?");
 
-let color = prompt("Qual'è il tuo colore preferito?");;
+let color = prompt("Qual'è il tuo colore preferito?");
 
 const password = name + surname + color;
 
